df9ae455895a94b43fd87dc159fea7355234437b
Alex <atomasllimos@gmail.com>
2024-04-25 07:59:53 +0200

    feat: Synchronizing URLs to FormState

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useRef } from 'react';
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
 import {
   Form,
-  Links,
+  NavLink,
   Meta,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+  Outlet,
+  Links,
+  useLoaderData,
+  useNavigation,
+} from '@remix-run/react';
+import appStylesHref from './app.css?url';
+import { createEmptyContact, getContacts } from './data';
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: appStylesHref }];
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
 
 export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (searchInputRef.current instanceof HTMLInputElement) {
+      searchInputRef.current.value = q ?? '';
+    }
+  }, [q]);
+
   return (
     <html lang="en">
       <head>
@@ -21,7 +53,9 @@ export default function App() {
           <div>
             <Form id="search-form" role="search">
               <input
+                defaultValue={q ?? ''}
                 id="q"
+                ref={searchInputRef}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
@@ -34,17 +68,39 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      to={`contacts/${contact.id}`}
+                      className={({ isActive, isPending }) => {
+                        if (isActive) return 'active';
+                        if (isPending) return 'pending';
+                      }}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{' '}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div id="detail" className={navigation.state === 'loading' ? 'loading' : ''}>
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
